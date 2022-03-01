2c0c0507dee8a0e1668834f66f6160d6074b426a
YorkNow <qiyao@51testing.com>
2022-03-01 18:08:56 +0800

    调整的调度框架

---
+++ samples/Job/Tasks/TestScheduler.cs
@@ -0,0 +1,14 @@
+﻿using Quartz;
+using System.Threading.Tasks;
+
+namespace Job.Tasks
+{
+    public class TestScheduler: Frame.Scheduler.IScheduler
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            int aaaa = 1;
+
+        }
+    }
+}
