ffbd684c1fed0e1f493f96182e4fbb3d205c24b2
YorkNow <qiyao@51testing.com>
2022-03-03 13:15:36 +0800

    调整架构

---
+++ samples/Study/AspCore/HttpRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace AspCore
+{
+    public class HttpRequest
+    {
+        public Uri Url { get; set; }
+        public NameValueCollection Headers { get; set; }
+
+        public Stream Body { get; set; }
+    }
+}
---
+++ samples/Study/AspCore/HttpResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Text;
+
+namespace AspCore
+{
+    public class HttpResponse
+    {
+        public int StatusCode { get; set; }
+        public NameValueCollection Headers { get; }
+        public Stream Body { get; }
+    }
+}
